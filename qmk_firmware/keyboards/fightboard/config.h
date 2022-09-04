5f02e6f0039dd5d518d40e8a77a417eead84550f
Carlos Ortiz <cartex1000@gmail.com>
2022-09-03 23:16:00 -0400

    new firmware for mcp23018

--- qmk_firmware/keyboards/fightboard/config.h
+++ qmk_firmware/keyboards/fightboard/config.h
@@ -18,11 +18,17 @@
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 8
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 16
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED_MASK 0U // Specify a optional status led which blinks when entering the bootloader
 
 #define RP2040_FLASH_GENERIC_03H
+
+#define F_SCL 400000
+
+#define I2C_DRIVER I2CD2
+#define I2C1_SDA_PIN GP6
+#define I2C1_SCL_PIN GP7
