51b2be8ec604707ffb961aa6a7d8bcfd5610dc8a
Carlos Ortiz <cartex1000@gmail.com>
2022-09-11 00:32:30 -0400

    add player 2 keymap

---
+++ qmk_firmware/keyboards/fightboard/keymaps/player2/keymap.c
@@ -0,0 +1,23 @@
+#include QMK_KEYBOARD_H
+
+void keyboard_post_init_user(void) {
+    // Customize these values to desired behaviour
+    debug_enable=true;
+    debug_matrix=true;
+    debug_keyboard=true;
+    //debug_mouse=true;
+}
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     *             │ - │ = │DEL│TAB│
+     *     ┌───┐   ┌───┬───┬───┬───┐
+     *     │ T │   │ 4 │ 5 │ 6 │ + │    Non letters are using
+     * ┌───┼───┼───┼───┼───┼───┼───┤    keypad variants to
+     * │ F │ G │ H │ 1 │ 2 │ 3 │ E │    differentiate w/ P1
+     * └───┴───┴───┴───┴───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_T, KC_H, KC_G, KC_F, KC_PENT, KC_P3, KC_P2, KC_P1, KC_PPLS, KC_P6, KC_P5, KC_P4, KC_MINS, KC_EQL, KC_DEL, KC_TAB
+    )
+};
