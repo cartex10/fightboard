6f545dcf00fd3edec0552f1481f51863c322f0ef
Carlos Ortiz <cartex10@bu.edu>
2022-12-16 01:55:04 -0500

    fix issues that arose with final install

--- qmk_firmware/keyboards/fightboard/v2/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/fightboard/v2/keymaps/default/keymap.c
@@ -43,28 +43,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 *  KC_A, KC_S, KC_D, KC_J, KC_K, KC_L, KC_SCLN
 	 */
 	[0] = LAYOUT(	// P1 Layer (Default)
-		KC_J, KC_K, KC_L, KC_SCLN, KC_D, KC_S, KC_A, KC_W, KC_MINS, KC_EQL, KC_BSPC, KC_ENT, KC_U, KC_I, KC_O, KC_P, TG(LAST_LAYER)
+		KC_J, KC_K, KC_L, KC_SCLN, KC_D, KC_S, KC_A, KC_W, KC_MINS, KC_EQL, KC_BSPC, KC_ENT, KC_U, KC_I, KC_O, KC_P, TG(LAST_LAYER+1)
 	),
     [1] = LAYOUT(	// P2 Layer
-        KC_Z, KC_X, KC_C, KC_V, KC_RGHT, KC_DOWN, KC_LEFT, KC_UP, KC_LBRC, KC_RBRC, KC_BSPC, KC_ENT, KC_B, KC_N, KC_M, KC_QUOT, TG(LAST_LAYER)
+        KC_Z, KC_X, KC_C, KC_V, KC_RGHT, KC_DOWN, KC_LEFT, KC_UP, KC_LBRC, KC_RBRC, KC_BSPC, KC_ENT, KC_B, KC_N, KC_M, KC_QUOT, TG(LAST_LAYER+1)
     ),
     [2] = LAYOUT(	// P3 Layer
-        KC_5, KC_6, KC_7, KC_8, KC_H, KC_G, KC_F, KC_T, KC_COMM, KC_DOT, KC_BSPC, KC_ENT, KC_1, KC_2, KC_3, KC_4, TG(LAST_LAYER)
+        KC_5, KC_6, KC_7, KC_8, KC_H, KC_G, KC_F, KC_T, KC_COMM, KC_DOT, KC_BSPC, KC_ENT, KC_1, KC_2, KC_3, KC_4, TG(LAST_LAYER+1)
     ),
     [3] = LAYOUT(	// P4 Layer
-        KC_P5, KC_P6, KC_P7, KC_P8, KC_Y, KC_R, KC_E, KC_Q, KC_NUBS, KC_PSLS, KC_BSPC, KC_ENT, KC_P1, KC_P2, KC_P3, KC_P4, TG(LAST_LAYER)
+        KC_P5, KC_P6, KC_P7, KC_P8, KC_Y, KC_R, KC_E, KC_Q, KC_NUBS, KC_PSLS, KC_BSPC, KC_ENT, KC_P1, KC_P2, KC_P3, KC_P4, TG(LAST_LAYER+1)
     ),
     [4] = LAYOUT(	// Alt1 Layer
-        KC_Z, KC_X, KC_C, KC_V, KC_RGHT, KC_DOWN, KC_LEFT, KC_UP, KC_ESC, KC_M, KC_TAB, KC_ENT, KC_A, KC_S, KC_D, KC_F, TG(LAST_LAYER)
+        KC_Z, KC_X, KC_C, KC_V, KC_RGHT, KC_DOWN, KC_LEFT, KC_UP, KC_ESC, KC_M, KC_TAB, KC_ENT, KC_A, KC_S, KC_D, KC_F, TG(LAST_LAYER+1)
     ),
     [5] = LAYOUT(	// Alt2 Layer
-        KC_LEFT, KC_DOWN, KC_RGHT, KC_TAB, KC_D, KC_S, KC_A, KC_W, KC_ESC, KC_R, KC_LCTL, KC_ENT, KC_Q, KC_UP, KC_E, KC_SPC, TG(LAST_LAYER)
+        KC_LEFT, KC_DOWN, KC_RGHT, KC_TAB, KC_D, KC_S, KC_A, KC_W, KC_ESC, KC_R, KC_LCTL, KC_ENT, KC_Q, KC_UP, KC_E, KC_SPC, TG(LAST_LAYER+1)
     ),
     [6] = LAYOUT(	// Alt3 Layer
-        KC_J, KC_K, KC_L, KC_DOWN, KC_F, KC_D, KC_S, KC_A, KC_ESC, KC_Q, KC_E, KC_ENT, KC_LEFT, KC_RGHT, KC_SCLN, KC_UP, TG(LAST_LAYER)
+        KC_J, KC_K, KC_L, KC_DOWN, KC_F, KC_D, KC_S, KC_A, KC_ESC, KC_Q, KC_E, KC_ENT, KC_LEFT, KC_RGHT, KC_SCLN, KC_UP, TG(LAST_LAYER+1)
     ),
 	[7] = LAYOUT(	// Config Layer (MUST ALWAYS BE LAST)
-		RGB_SPD, RGB_M_R, RGB_M_SW, REACTIVE_FULL_RGB, RGB_HUI, RGB_VAD, RGB_HUD, RGB_VAI, PREV_LAYER, NEXT_LAYER, KC_BSPC, KC_ESC, RGB_SPI, RGB_M_P, RGB_M_B, REACTIVE_SIMPLE_RGB, TG(LAST_LAYER)
+		RGB_SPD, RGB_M_R, RGB_M_SW, REACTIVE_FULL_RGB, RGB_HUI, RGB_VAD, RGB_HUD, RGB_VAI, PREV_LAYER, NEXT_LAYER, KC_BSPC, KC_ESC, RGB_SPI, RGB_M_P, RGB_M_B, REACTIVE_SIMPLE_RGB, TG(LAST_LAYER+1)
 	),
 };
 
@@ -85,14 +85,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			}
 			return false;
 		case REACTIVE_SIMPLE_RGB:
-			if (record->event.pressed) { 
-				rgb_matrix_mode(RGB_MATRIX_SOLID_REACTIVE_SIMPLE);
-			}
+			if (record->event.pressed) { rgb_matrix_mode(RGB_MATRIX_SOLID_REACTIVE_SIMPLE); }
 			return false;
 		case REACTIVE_FULL_RGB:
-			if (record->event.pressed) { 
-				rgb_matrix_mode(RGB_MATRIX_SOLID_REACTIVE);
-			}
+			if (record->event.pressed) { rgb_matrix_mode(RGB_MATRIX_SOLID_REACTIVE); }
 			return false;
 		default:
 			return true; // Process all other keycodes normally
--- qmk_firmware/keyboards/fightboard/v2/matrix.c
+++ qmk_firmware/keyboards/fightboard/v2/matrix.c
@@ -5,14 +5,11 @@
 #ifndef NO_PRINT
 #include "print.h"
 #endif
-
-#define MCP_ADDR (0x20)
 #ifndef LAST_LAYER
 #define LAST_LAYER 6
 #endif
-//#define mcp_keys { 'W', 'A', 'S', 'D', 'U', 'I', 'O', 'P', 'J', 'K', 'L', ';', '-', '=', "BKSP", "ESC"}
-//#define mcp_pins { A7 , A6 , A5 , A4 , B4 , B5 , B6 , B7 , A0 , A1 , A2 , A3 , B0 , B1 ,   B2  ,   B3 }
 
+#define MCP_ADDR (0x20)
 extern matrix_row_t matrix[MATRIX_ROWS];
 extern int LAYER;
 extern const int indicatorHSV[LAST_LAYER+2][3];
@@ -65,8 +62,8 @@ bool matrix_scan_custom(uint16_t current_matrix[]) {
 				else { hSOCD = true; }
 				#endif
 				// Change all leds if lower layer is changed
-				if (LAST_LAYER == get_highest_layer(layer_state|default_layer_state)) {
-					if (((temp | 0xfbff) == 0xffff) || ((temp | 0xfdff) == 0xffff)) {
+				if (LAST_LAYER + 1 == get_highest_layer(layer_state|default_layer_state)) {
+					if (((temp | 0xfeff) == 0xffff) || ((temp | 0xfdff) == 0xffff)) {
 						HSV layerHSV = { indicatorHSV[LAYER][0], indicatorHSV[LAYER][1], indicatorHSV[LAYER][2] };
 						RGB layerRGB = hsv_to_rgb(layerHSV);
 						rgb_matrix_set_color_all(layerRGB.r, layerRGB.g, layerRGB.b);
@@ -86,7 +83,7 @@ bool matrix_scan_custom(uint16_t current_matrix[]) {
 
 	if (LEDpin) { LEDHoldCount += 1; }
 	else { LEDHoldCount = 0; }
-	if (LEDHoldCount == 1500) { rgb_matrix_toggle(); }
+	if (LEDHoldCount == 3000) { rgb_matrix_toggle(); }
 	
 	//uprintf("current_matrix = %x\n\n\n\n", *current_matrix);
 	return matrix_has_changed;
@@ -95,7 +92,7 @@ bool matrix_scan_custom(uint16_t current_matrix[]) {
 
 led_config_t g_led_config = { {
 	// Key Matrix to LED Index
-	{ 8, 7, 6, 5, 9, 10, 11, 12, NO_LED, NO_LED, NO_LED, NO_LED, 1, 2, 3, 4, 13 }
+	{ 7, 6, 5, 4, 8, 9, 10, 11, NO_LED, NO_LED, NO_LED, NO_LED, 0, 1, 2, 3, 12 }
 }, {
 	// LED Index to Physical Position
 	//x = 224 / (NUMBER_OF_COLS - 1) * COL_POSITION
@@ -103,13 +100,15 @@ led_config_t g_led_config = { {
 	{ 112, 42 }, { 139, 42 }, { 166, 42 }, { 193, 42 }, { 204, 19 }, { 177, 19 }, { 150, 19 }, { 123, 19 }, { 72, 19 }, { 45, 19 }, { 18, 19 }, { 34, 42 }, { 45, 51 }
 }, {
 	// LED Index to Flag
-	LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_INDICATOR, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_INDICATOR
+	LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_INDICATOR
 } };
 
 bool rgb_matrix_indicators_kb(void) {
 	int layer = get_highest_layer(layer_state|default_layer_state);
-	HSV layerHSV = { indicatorHSV[layer][0], indicatorHSV[layer][1], indicatorHSV[layer][2] };
+	int value = 80;
+	if (layer == LAST_LAYER + 1) { value = 0; } 
+	HSV layerHSV = { indicatorHSV[layer][0], indicatorHSV[layer][1], value };
 	RGB layerRGB = hsv_to_rgb(layerHSV);
-	rgb_matrix_set_color(7, layerRGB.r, layerRGB.g, layerRGB.b);
+	rgb_matrix_set_color(12, layerRGB.r, layerRGB.g, layerRGB.b);
 	return true;
 }
